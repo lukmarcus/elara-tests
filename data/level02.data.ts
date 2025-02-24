e6eb5c76c701e95cb740a8a6521f678d55760767
Marek Szumny <lukmarcus@gmail.com>
2025-02-25 00:28:59 +0100

    level 02 data added

---
+++ data/level02.data.ts
@@ -0,0 +1,24 @@
+export const level02Start = `// Try collecting an energy cell before moving to the goal.
+
+// CHANGE THE CODE BELOW
+move_forward(4);
+turn_left();
+move_forward(4);`;
+
+export const level02Solution = `// Try collecting an energy cell before moving to the goal.
+
+// CHANGE THE CODE BELOW
+move_forward(5);
+turn_left();
+turn_left();
+move_forward(1);
+turn_right();
+move_forward(4);`;
+
+export const level02Challenge = `// Try collecting an energy cell before moving to the goal.
+
+// CHANGE THE CODE BELOW
+move_forward(5);
+move_backward(1);
+turn_left();
+move_forward(4);`;
