97096a80cb166e62ee51940e3657d40b1ac28c2e
Marek Szumny <lukmarcus@gmail.com>
2025-02-25 20:09:39 +0100

    level 03 data added

---
+++ data/level03.data.ts
@@ -0,0 +1,15 @@
+export const level03Start = `loop {
+  move_forward(1);
+  turn_left();
+  move_forward(1);
+  // CHANGE THE CODE BELOW
+  turn_left();
+}`;
+
+export const level03Solution = `loop {
+  move_forward(1);
+  turn_left();
+  move_forward(1);
+  // CHANGE THE CODE BELOW
+  turn_right();
+}`;
