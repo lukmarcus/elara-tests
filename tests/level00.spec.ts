345fdb8af9d121bb63a85d812acdc1dfae021312
Marek Szumny <lukmarcus@gmail.com>
2025-03-06 23:51:44 +0100

    level test changes

---
+++ tests/level00.spec.ts
@@ -0,0 +1,24 @@
+import { HubPage } from '../pages/hub.page';
+import { StartPage } from '../pages/start.page';
+import { useStorageState } from '../utils/storage-state.util';
+import { expect } from '@playwright/test';
+
+const test = useStorageState('level-00-ready');
+
+test.describe('Level 00', async () => {
+  let hubPage: HubPage;
+
+  test.beforeEach(async ({ page }) => {
+    const startPage = new StartPage(page);
+    await startPage.goto();
+    const mainMenuPage = await startPage.clickPlayButton();
+    hubPage = await mainMenuPage.clickContinueButton();
+  });
+
+  test('Level 00', async () => {
+    await expect(hubPage.monitorBox.locator('.chakra-text')).toHaveText(
+      'New Levels!',
+    );
+    await expect(hubPage.journalBox).toBeEnabled();
+  });
+});
