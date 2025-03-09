46c0b761951dca6dff79da23e29e84ca34b7f253
Marek Szumny <lukmarcus@gmail.com>
2025-03-09 20:34:24 +0100

    load storage state function added

--- utils/storage-state.util.ts
+++ utils/storage-state.util.ts
@@ -1,13 +1,25 @@
-import { Page, test as base } from '@playwright/test';
+import { Browser, BrowserContext, Page, test as base } from '@playwright/test';
 import path from 'path';
 
-export async function saveStorageState(page: Page, fileName: string) {
+export async function saveStorageState(
+  page: Page,
+  fileName: string,
+): Promise<void> {
   await page
     .context()
     .storageState({ path: path.join(process.cwd(), `saves/${fileName}.json`) });
 }
 
-export function useStorageState(fileName: string) {
+export async function loadStorageState(
+  browser: Browser,
+  file: string,
+): Promise<BrowserContext> {
+  return await browser.newContext({
+    storageState: path.join(process.cwd(), `saves/${file}.json`),
+  });
+}
+
+export function useStorageState(fileName: string): typeof base {
   return base.extend({
     storageState: path.resolve(process.cwd(), `saves/${fileName}.json`),
   });
