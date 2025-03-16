e7dbf162f91be32854ad7f25f4061d6475138ec4
Marek Szumny <lukmarcus@gmail.com>
2025-03-16 23:40:50 +0100

    help data added

---
+++ data/help.data.ts
@@ -0,0 +1,11 @@
+export const level00Help01Title = 'How to run code';
+export const level00Help01Description =
+  'Press "Deploy" and then "Play" to run your code. The code will cause the rover to move around and do things.';
+
+export const level00Help02Title = 'Objectives';
+export const level00Help02Description =
+  'You can find the current objective near the top of the screen. Usually you will need to move the rover to the goal, but sometimes you may need to do something else.';
+
+export const level00Help03Title = 'Error messages';
+export const level00Help03Description =
+  'Even the best coders get errors sometimes! If you hover over the red dot, you can see more information about the error.';
