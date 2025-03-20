fe455382129d878bc2d3e7e3a2751e3082a6cc69
Marek Szumny <lukmarcus@gmail.com>
2025-03-20 22:46:47 +0100

    help data updated with images src0

--- data/help.data.ts
+++ data/help.data.ts
@@ -1,17 +1,32 @@
 const level00Help01Title = 'How to run code';
 const level00Help01Description =
   'Press "Deploy" and then "Play" to run your code. The code will cause the rover to move around and do things.';
+const level00Help01Image = '/assets/how_to_run_code';
 
 const level00Help02Title = 'Objectives';
 const level00Help02Description =
   'You can find the current objective near the top of the screen. Usually you will need to move the rover to the goal, but sometimes you may need to do something else.';
+const level00Help02Image = '/assets/where_to_find_objectives';
 
 const level00Help03Title = 'Error messages';
 const level00Help03Description =
   'Even the best coders get errors sometimes! If you hover over the red dot, you can see more information about the error.';
+const level00Help03Image = '/assets/how_to_see_errors';
 
 export const level00HelpData = [
-  { title: level00Help01Title, description: level00Help01Description },
-  { title: level00Help02Title, description: level00Help02Description },
-  { title: level00Help03Title, description: level00Help03Description },
+  {
+    title: level00Help01Title,
+    description: level00Help01Description,
+    image: level00Help01Image,
+  },
+  {
+    title: level00Help02Title,
+    description: level00Help02Description,
+    image: level00Help02Image,
+  },
+  {
+    title: level00Help03Title,
+    description: level00Help03Description,
+    image: level00Help03Image,
+  },
 ];
