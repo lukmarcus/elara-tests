c98ab05a8fa15b256a9c18ad0d1d376c231f9dbb
Marek Szumny <lukmarcus@gmail.com>
2025-03-24 22:54:00 +0100

    hints data added

---
+++ data/hints.data.ts
@@ -0,0 +1,10 @@
+const level00Hints01 =
+  'Find the line of code that says move_forward(1);. Try changing the number 1 to a different number.';
+
+const level00Hints02 =
+  "The turn_right function doesn't expect any inputs, so you don't need to put anything in between the parentheses. (You'll learn more about function inputs soon.)";
+
+export const level00HintsData = {
+  hint01: level00Hints01,
+  hint02: level00Hints02,
+};
